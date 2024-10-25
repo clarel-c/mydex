656329ac75096f7583a51d577018c86d6759a9cb
clarel <ccatherine@utm.ac.mu>
2024-10-25 22:11:48 +0400

    Initial Tests

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,8 +4,23 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 contract Token {
-    string public name = "MBTBA Token";
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply){
+        //Code written here is executed when the contract is put on the blockchain
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10 ** decimals);
+    }
 }
+
+
+
+
+
 
 
 
---
+++ test/Token.js
@@ -0,0 +1,66 @@
+const { ethers } = require("hardhat")
+const { expect } = require("chai")
+
+const weiValue = function (num) {
+    return ethers.utils.parseUnits(num.toString(), "ether")
+}
+
+describe("Token Contract", function () {
+    let token
+
+    beforeEach( async function () {
+        const Token = await ethers.getContractFactory("Token")
+        token = await Token.deploy("MBTBA Token", "MBTBA", 100000)
+    })
+
+    describe("Deployment", function () {
+        const _name = "MBTBA Token"
+        const _symbol = "MBTBA"
+        const _decimals = 18
+        const _totalSupply = weiValue(100000)
+
+        it("has the correct name", async function () {
+            const name = await token.name()
+            expect(name).to.equal(_name)
+        })
+    
+        it("has the correct symbol", async function () {
+            const symbol = await token.symbol()
+            expect(symbol).to.equal(_symbol)
+        })
+    
+        it("has the correct no of decimals", async function () {
+            let decimals = await token.decimals()
+            expect(decimals).to.equal(_decimals)
+        })
+    
+        it("has the correct total supply", async function () {
+            const totalSupply = await token.totalSupply()
+            expect(totalSupply).to.equal(_totalSupply)
+        })
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
