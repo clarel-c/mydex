a5f3fcc7d136f0b7ac5778f5f98bb32beb204983
clarel <ccatherine@utm.ac.mu>
2024-11-08 13:24:59 +0400

    6. Deposit and withdraw tokens, Make Orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -5,13 +5,45 @@ import "./Token.sol";
 import "hardhat/console.sol";
 
 contract Exchange {
-    address public feeAccount;   
-    uint256 public feePercent; 
+    address public feeAccount;
+    uint256 public feePercent;
+    uint256 public ordersCounter;
 
     mapping(address => mapping(address => uint256)) public tokensBalance;
+    mapping(uint256 => _Order) public orders;
 
-    event Deposit(address _token, address _user, uint256 _amount, uint256 _balance);
-    event Withdraw(address _token, address _user, uint256 _amount, uint256 _balance);
+    event Deposit(
+        address _token,
+        address _user,
+        uint256 _amount,
+        uint256 _balance
+    );
+    event Withdraw(
+        address _token,
+        address _user,
+        uint256 _amount,
+        uint256 _balance
+    );
+
+    event Order(
+        uint256 id,
+        address user,
+        address tokenBuy,
+        uint256 amountBuy,
+        address tokenSell,
+        uint256 amountSell,
+        uint256 timestamp
+    );
+
+    struct _Order {
+        uint256 id;
+        address user;
+        address tokenBuy;
+        uint256 amountBuy;
+        address tokenSell;
+        uint256 amountSell;
+        uint256 timestamp;
+    }
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -24,7 +56,12 @@ contract Exchange {
         // 2. Update the user balance on the exchange for that user and for that token
         tokensBalance[_token][msg.sender] += _amount;
         // 3. Emit a Deposit Event which the UI can later use.
-        emit Deposit(_token, msg.sender, _amount, tokensBalance[_token][msg.sender]);
+        emit Deposit(
+            _token,
+            msg.sender,
+            _amount,
+            tokensBalance[_token][msg.sender]
+        );
     }
 
     function withdrawToken(address _token, uint256 _amount) public {
@@ -33,48 +70,53 @@ contract Exchange {
         // 1. Transfer the token back to the user
         Token(_token).transfer(msg.sender, _amount);
         // 2. Update the user balance on the exchange
-        tokensBalance[_token][msg.sender] -= _amount;       
+        tokensBalance[_token][msg.sender] -= _amount;
         // 3. Emit a Withdraw event
-        emit Withdraw(_token, msg.sender, _amount, tokensBalance[_token][msg.sender]);
+        emit Withdraw(
+            _token,
+            msg.sender,
+            _amount,
+            tokensBalance[_token][msg.sender]
+        );
     }
 
-    function balanceOf(address _token, address _user) public view returns (uint256) {
+    function balanceOf(
+        address _token,
+        address _user
+    ) public view returns (uint256) {
         return tokensBalance[_token][_user];
     }
+
+    function makeOrder(
+        address _tokenBuy,
+        uint256 _amountBuy,
+        address _tokenSell,
+        uint256 _amountSell
+    ) public {
+        ordersCounter++;
+
+        require(balanceOf(_tokenSell, msg.sender) >= _amountSell);
+
+        orders[ordersCounter] = _Order(
+            ordersCounter,
+            msg.sender,
+            _tokenBuy,
+            _amountBuy,
+            _tokenSell,
+            _amountSell,
+            block.timestamp
+        );
+
+        emit Order(
+            ordersCounter,
+            msg.sender,
+            _tokenBuy,
+            _amountBuy,
+            _tokenSell,
+            _amountSell,
+            block.timestamp           
+        );
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
--- test/Exchange.js
+++ test/Exchange.js
@@ -6,7 +6,7 @@ const weiValue = function (num) {
 }
 
 describe("Exchange Contract", function () {
-    let accounts, deployer, feeAccount, exchange, user1, token1, transaction, transactionReceipt
+    let accounts, deployer, feeAccount, exchange, user1, token1, token2, transaction, transactionReceipt
     const feePercent = 1
 
     beforeEach(async function () {
@@ -20,11 +20,13 @@ describe("Exchange Contract", function () {
 
         const Token = await ethers.getContractFactory("Token")
         token1 = await Token.deploy("MBTBA Token", "MBTBA", 100000)
+        token2 = await Token.deploy("UTM Coin", "UTMC", 100000)
 
         // 1. Send tokens from the deployer to user1 using the transfer function
         transaction = await token1.connect(deployer).transfer(user1.address, weiValue(20000))
         await transaction.wait()
-        // 2. Have user1 approve the exchange as a spender.
+
+        // 2. Have user1 approve the exchange as a spender for token 1.
         transaction = await token1.connect(user1).approve(exchange.address, weiValue(10000))
         await transaction.wait()
     })
@@ -98,7 +100,44 @@ describe("Exchange Contract", function () {
             await expect(exchange.connect(user1).withdrawToken(token1.address, weiValue(4000))).not.to.be.reverted
         })
     })
+
+    describe("Making Orders", async function () {
+
+        beforeEach(async function () {
+
+            transaction = await exchange.connect(user1).depositToken(token1.address, weiValue(7000))
+            await transaction.wait()
+
+            transaction = await exchange.connect(user1).makeOrder(token2.address, weiValue(200), token1.address, weiValue(100))
+            transactionReceipt = await transaction.wait()
+        })
+
+        it("tracks a new order and emits an order event", async function () {
+            expect(await exchange.ordersCounter()).to.equal(1)
+
+            const firstOrder = await exchange.connect(user1).orders(1)
+            const timestamp = firstOrder.timestamp
+
+            expect(transactionReceipt.events[0].event).to.equal("Order")
+            expect(transactionReceipt.events[0].args.id).to.equal(1)
+            expect(transactionReceipt.events[0].args.user).to.equal(user1.address)
+            expect(transactionReceipt.events[0].args.tokenBuy).equal(token2.address)
+            expect(transactionReceipt.events[0].args.amountBuy).to.equal(weiValue(200))
+            expect(transactionReceipt.events[0].args.tokenSell).equal(token1.address)
+            expect(transactionReceipt.events[0].args.amountSell).to.equal(weiValue(100))
+            expect(transactionReceipt.events[0].args.timestamp).to.equal(timestamp)
+        })  
+        
+        it("rejects orders when user has insufficient balance", async function () {
+            await expect(exchange.connect(user1).makeOrder(token2.address, weiValue(14002), token1.address, weiValue(7001))).to.be.reverted
+            await expect(exchange.connect(user1).makeOrder(token2.address, weiValue(14000), token1.address, weiValue(7000))).not.to.be.reverted
+        })
+    })
 })
+
+
+
+
 
 
 
